e893ae7db0610fe1bba8d48ded4d3c5e586f6f1a
DongCX-LDHSP <52726994+DongCX-LDHSP@users.noreply.github.com>
2021-04-24 10:54:16 +0800

    添加：对Socket的调用初始化

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "MainWindow.h"
 #include <QApplication>
+#include "Socket/tcp_server.h"
 
 int main(int argc, char *argv[])
 {
@@ -7,5 +8,8 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
 
+    quint16 port = 8888;
+    tcp_server server;
+    server.init_server(port);
     return a.exec();
 }
