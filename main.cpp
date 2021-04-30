98a304c66cde92e4e4ac470c3afb93552b353572
DongCX-LDHSP <52726994+DongCX-LDHSP@users.noreply.github.com>
2021-04-30 09:17:17 +0800

    删除：主程序中关于Socket的代码

--- main.cpp
+++ main.cpp
@@ -1,15 +1,10 @@
 #include "MainWindow.h"
 #include <QApplication>
-#include "Socket/tcp_server.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-
-    quint16 port = 8888;
-    tcp_server server;
-    server.init_server(port);
     return a.exec();
 }
